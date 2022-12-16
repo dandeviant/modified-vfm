e235e1b86a6b4fa1d5830cb5b49b57de66b7b885
Muhammad Danial <muhammaddanial63@gmail.com>
2022-12-16 09:19:32 +0800

    first commit

---
+++ vfm-admin/_content/users/users.php
@@ -0,0 +1,17 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$ifFgY3U0$SMHpk9zOSD3t6Z2upQAKn0',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'daniel',
+    'pass' => '$1$jSSrn6Y6$J9IuGnmkpX8JCIcwgAiqZ1',
+    'role' => 'contributor',
+    'dir' => '["daniel"]',
+  ),
+);
